4141ba17b9d20375b17808d2e48568ce043f1b60
dariigum <darigamusa1@gmail.com>
2024-05-02 18:51:26 +0500

    2.1. created MyTestingClass

---
+++ src/MyTestingClass.java
@@ -0,0 +1,16 @@
+import java.util.Objects;
+import java.util.Random;
+
+public class MyTestingClass {
+
+    private int id;
+
+
+    public MyTestingClass(String string) {
+        this.id = id;
+
+
+    }
+
+
+}
